228302916bbf951f20b8a4ad5ac54bade5fce64f
Erik <ol.erik0107@gmail.com>
2024-10-16 08:25:40 -0300

    chore: adjusting logs

--- src/logs/log.config.ts
+++ src/logs/log.config.ts
@@ -1,6 +1,11 @@
 import fs from 'node:fs'
 import path from 'node:path'
 
+import { FastifyLoggerOptions } from 'fastify'
+import { PinoLoggerOptions } from 'fastify/types/logger'
+
+type LoggerType = FastifyLoggerOptions & PinoLoggerOptions
+
 const logDirectory = path.join('src', 'logs')
 const logFile = path.join(logDirectory, 'index.log')
 
@@ -8,8 +13,10 @@ if (!fs.existsSync(logDirectory)) {
   fs.mkdirSync(logDirectory)
 }
 
-export const logger = {
+export const logger: LoggerType = {
   file: logFile,
+  enabled: true,
+  timestamp: true,
   transport: {
     target: 'pino-pretty',
     options: {
