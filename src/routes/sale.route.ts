65679c76102bc9501e72e224dd02e3375df3c24a
Erik <ol.erik0107@gmail.com>
2024-10-18 18:16:50 -0300

    feat: creating sale controller and route

--- src/routes/sale.route.ts
+++ src/routes/sale.route.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify'
 
+import { cancelSale } from '@/controllers/sale/cancel'
 import { createSale } from '@/controllers/sale/create'
 
 export const saleRoutes = async (app: FastifyInstance) => {
   app.post('/sale-create', createSale)
+  app.put('/sale-cancel', cancelSale)
 }
